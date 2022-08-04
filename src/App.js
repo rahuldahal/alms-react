f33398f3cf51298e18dd70c01fcea7220bcbce7e
Rahul Dahal <rdaahal@gmail.com>
2022-08-04 13:30:49 +0545

    chore: setup react-router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import "./App.css";
 import React from "react";
 import Nav from "./components/Nav";
 import Landing from "./Screens/Landing";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Landing />
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/dashboard" element={<h1>Dahsboard</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
