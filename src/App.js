d28f9ca59561a5a062b239e1a3688f1bcfc2d3f2
rahuldahal <rdaahal@gmail.com>
2023-03-27 10:30:52 +0545

    feat: warning iff the response is delayed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
-import "./App.css";
+import './App.css';
 
-import { Spin } from "antd";
-import HODs from "./Screens/HODs";
-import Nav from "./components/Nav";
-import useAuth from "./hooks/useAuth";
-import Landing from "./Screens/Landing";
-import Courses from "./Screens/Courses";
-import Layout from "./components/Layout";
-import Students from "./Screens/Students";
-import Teachers from "./Screens/Teachers";
-import Subjects from "./Screens/Subjects";
-import Attendance from "./Screens/Attendance";
-import { getAuthStatus } from "./services/user";
-import { Routes, Route } from "react-router-dom";
-import React, { useEffect, useState } from "react";
-import PrivateRoutes from "./components/PrivateRoutes";
-import StudentsOfASubject from "./Screens/StudentsOfASubject";
-import { StudentAttendance } from "./Screens/StudentAttendance";
-import AttendanceCalendar from "./components/AttendanceCalendar";
+import { Spin, notification } from 'antd';
+import HODs from './Screens/HODs';
+import Nav from './components/Nav';
+import useAuth from './hooks/useAuth';
+import Landing from './Screens/Landing';
+import Courses from './Screens/Courses';
+import Layout from './components/Layout';
+import Students from './Screens/Students';
+import Teachers from './Screens/Teachers';
+import Subjects from './Screens/Subjects';
+import Attendance from './Screens/Attendance';
+import { getAuthStatus } from './services/user';
+import { Routes, Route } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import PrivateRoutes from './components/PrivateRoutes';
+import StudentsOfASubject from './Screens/StudentsOfASubject';
+import { StudentAttendance } from './Screens/StudentAttendance';
+import AttendanceCalendar from './components/AttendanceCalendar';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const { auth, setAuth } = useAuth();
+  const [api, contextHolder] = notification.useNotification();
+
+  // notification setup
+  const openNotificationWithIcon = () => {
+    api.warning({
+      message: 'Warning!',
+      description:
+        'Looks like the server has gone to sleep. So, the initial load might take a bit  longer.',
+      duration: 8,
+    });
+  };
 
   useEffect(() => {
     (async () => {
@@ -38,11 +49,24 @@ function App() {
     })();
   }, []);
 
+  useState(() => {
+    // when server goes to sleep on production
+    setTimeout(() => {
+      console.log({ isLoading });
+      if (isLoading) {
+        openNotificationWithIcon();
+      }
+    }, 1500);
+  }, [isLoading]);
+
   return (
     <>
       <Nav loading={isLoading} />
       {isLoading ? (
-        <Spin size="large" className="apiLoader" />
+        <>
+          <Spin size="large" className="apiLoader" />
+          {contextHolder}
+        </>
       ) : (
         <Routes>
           <Route path="/" element={<Layout />}>
@@ -51,14 +75,14 @@ function App() {
 
             {/* Protected Route */}
             <Route
-              element={<PrivateRoutes allowedRoles={["PRINCIPAL", "HOD"]} />}
+              element={<PrivateRoutes allowedRoles={['PRINCIPAL', 'HOD']} />}
             >
               <Route path="/courses" element={<Courses />} exact />
             </Route>
 
             <Route
               element={
-                <PrivateRoutes allowedRoles={["PRINCIPAL", "HOD", "TEACHER"]} />
+                <PrivateRoutes allowedRoles={['PRINCIPAL', 'HOD', 'TEACHER']} />
               }
             >
               <Route path="/attendances" element={<Attendance />} exact />
@@ -66,13 +90,13 @@ function App() {
 
             <Route
               element={
-                <PrivateRoutes allowedRoles={["PRINCIPAL", "HOD", "TEACHER"]} />
+                <PrivateRoutes allowedRoles={['PRINCIPAL', 'HOD', 'TEACHER']} />
               }
             >
               <Route path="/subjects" element={<Subjects />} exact />
             </Route>
 
-            <Route element={<PrivateRoutes allowedRoles={["TEACHER"]} />}>
+            <Route element={<PrivateRoutes allowedRoles={['TEACHER']} />}>
               <Route
                 path="/subjects/students"
                 element={<StudentsOfASubject />}
@@ -80,7 +104,7 @@ function App() {
               />
             </Route>
 
-            <Route element={<PrivateRoutes allowedRoles={["STUDENT"]} />}>
+            <Route element={<PrivateRoutes allowedRoles={['STUDENT']} />}>
               <Route
                 path="/my-attendance"
                 element={<StudentAttendance />}
@@ -88,7 +112,7 @@ function App() {
               />
             </Route>
 
-            <Route element={<PrivateRoutes allowedRoles={["STUDENT"]} />}>
+            <Route element={<PrivateRoutes allowedRoles={['STUDENT']} />}>
               <Route
                 path="/attendance-calendar"
                 element={<AttendanceCalendar />}
@@ -98,19 +122,19 @@ function App() {
 
             <Route
               element={
-                <PrivateRoutes allowedRoles={["PRINCIPAL", "HOD", "TEACHER"]} />
+                <PrivateRoutes allowedRoles={['PRINCIPAL', 'HOD', 'TEACHER']} />
               }
             >
               <Route path="/users/students" element={<Students />} exact />
             </Route>
 
             <Route
-              element={<PrivateRoutes allowedRoles={["PRINCIPAL", "HOD"]} />}
+              element={<PrivateRoutes allowedRoles={['PRINCIPAL', 'HOD']} />}
             >
               <Route path="/users/teachers" element={<Teachers />} exact />
             </Route>
 
-            <Route element={<PrivateRoutes allowedRoles={["PRINCIPAL"]} />}>
+            <Route element={<PrivateRoutes allowedRoles={['PRINCIPAL']} />}>
               <Route path="/users/hods" element={<HODs />} exact />
             </Route>
 
